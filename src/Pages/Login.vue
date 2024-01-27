70499b90ecbec0ed0711db41329c3dedd692fb82
Edmario Oliveira <barrigacraft@hotmail.com>
2024-01-27 00:22:50 -0300

    mudança no login

--- src/Pages/Login.vue
+++ src/Pages/Login.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="container">
-    <div class="form">
-      <div class="header">
-        <h5>Login</h5>
+  <div class="main-login">
+    <div class="container-form">
+      <div class="form-login">
+        <div style="text-align: center;" class="text-content-login">
+            <h5>SGA</h5>
+            <img style="width: 80px" src="../assets/img/logo.png" alt="">
+        </div>
+        <Input :id="'email'" :type="'text'" v-model="email" :label="'E-mail'" />
+        <Input
+          :id="'password'"
+          :type="'password'"
+          v-model="senha"
+          :label="'Senha'"
+        />
+        <Button @click="login" :text="'Entrar'" :icon="'input'" />
       </div>
-      <Input :id="'email'" :type="'text'" v-model="email" :label="'E-mail'" />
-      <Input
-        :id="'password'"
-        :type="'password'"
-        v-model="senha"
-        :label="'Senha'"
-      />
-      <Button @click="login" :text="'Entrar'" :icon="'input'" />
+      <div class="image-container">
+        <img src="../assets/img/login_image.jpg" alt="" />
+      </div>
     </div>
   </div>
 </template>
@@ -61,15 +67,41 @@ export default {
 </script>
 
 <style scoped>
-.form {
-  text-align: center;
-  padding: 10% 30% 0% 30%;
+.main-login {
+  background-color: var(--primary);
+  width: 100% !important;
+  height: 100vh !important;
+  justify-content: center;
+  display: flex;
+}
+
+.container-form {
+  border-radius: 15px;
+  margin-top: 10%;
+  height: 400px;
+  background: white;
+  width: 800px;
+  justify-content: center;
+  display: flex;
+}
+
+.form-login {
+  padding-left: 35px;
+  width: 50%;
+}
+
+.image-container {
+  width: 50%;
+}
+
+.image-container img {
+  width: 380px;
 }
 
 @media (max-width: 760px) {
-  .form {
-    padding: 20% 10% 0% 10%;
-  }
+}
+.active label {
+  color: var(--primary) !important;
 }
 </style>
 
